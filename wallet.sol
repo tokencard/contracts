43836b1f8ddd1116edc1ed0a5f1d85e351af921e
nostdm <nostdm@users.noreply.github.com>
2018-05-22 14:16:21 +0100

    Initial wallet implementation

--- wallet.sol
+++ wallet.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.23;
+pragma solidity ^0.4.24;
 
 
 // The Token interface is a subset of the ERC20 specification.
@@ -14,44 +14,106 @@ contract Wallet {
     event Deposit(address from, uint amount);
 
     // Owner of the wallet.
+    address public controller;
+    address public newController;
     address public owner;
+    address public newOwner;
+    address[] public whitelist;
 
-    // Construct a wallet owned by the sender with the given controller.
-    constructor() public {
-        owner = msg.sender;
+    // Construct a wallet with an owner and a controller.
+    constructor(address o, address c) public {
+        owner = o;
+        controller = c;
     }
 
-    // Wallets may be topped up from any source, so this contract must be payable by anyone.
+    // Executable only by the specified address.
+    modifier only(address a) {
+        require(msg.sender == a);
+        _;
+    }
+
+    // Executable by either of the two addresses.
+    modifier either(address a, address b) {
+        require(msg.sender == a || msg.sender == b);
+        _;
+    }
+
+    // Ether can be deposited from any source, so this contract must be payable by anyone.
     function () external payable {
         if (msg.value > 0) {
             emit Deposit(msg.sender, msg.value);
         }
     }
 
-    // Balance returns the amount of a token owned by the contract.
+    // Returns the amount of a token owned by the contract.
     function balance(address token) public constant returns (uint) {
-        if (token != 0x0) {
-            return Token(token).balanceOf(this);
-        } else {
-            return address(this).balance;
-        }
+        return Token(token).balanceOf(this);
+    }
+    // Returns the amount of ether owned by the contract.
+    function balance() public constant returns (uint) {
+        return address(this).balance;
     }
 
-    // Change the owner to the given address.
-    function changeOwner(address o) public {
-        owner = o;
+    // Transfer contract ownership to another Ethereum address.
+    function transferOwnership(address account) public only(owner) {
+        newOwner = account;
     }
 
-    // Transfer funds to an address.
-    function transfer(address to, address token, uint amount) public {
+    // Accept contract ownership.
+    function acceptOwnership() public only(newOwner) {
+        owner = newOwner;
+    }
+
+    // Add an address to a whitelist.
+    // TODO: This should be multi sig to prevent attacker changing the addresses.
+    function addWhitelistAddress(address a) public only(owner) {
+        whitelist.push(a);
+    }
+
+    // TODO: This should be multi sig to prevent attacker changing the addresses.
+    function removeWhitelistAddress(address a) public only(owner) {
+        whitelist.push(a);
+    }
+
+    // Transfer asset to an address.
+    function transfer(address to, address token, uint amount) public only(owner) {
         if (amount == 0) {
             return;
         }
-        if (token != 0x0) {
-            require(Token(token).transfer(to, amount));
-        } else {
-            to.transfer(amount);
-        }
+        require(Token(token).transfer(to, amount));
+
         emit Transfer(to, token, amount);
+    }
+
+    // Transfer ether to an address.
+    function transfer(address to, uint amount) public only(owner) {
+        if (amount == 0) {
+            return;
+        }
+        to.transfer(amount);
+
+        emit Transfer(to, 0x0, amount);
+    }
+
+    // TODO: We have a list of private addresses, these addresses should be able to top up user's gas and cover the transaction cost.
+    function transferControl(address a) public only(controller) {
+        newController = a;
+    }
+
+    function acceptControl() public only(newController) {
+        controller = newController;
+    }
+
+
+    // Top up owner's gas.
+    // TODO: Callable by the controller address and the owner address.
+    function addGas() public either(owner, controller) {
+
+        // TODO: Check if the owner already has enough gas.
+
+        // TODO: Set cap to how much ether can be transferred.
+
+        // TODO: Transfer ether to owner's account
+
     }
 }
