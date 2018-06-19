a9d7d43535345d40c061a9fc55a4e254362e682d
nostdm <nostdm@users.noreply.github.com>
2018-06-19 14:36:12 +0100

    Fix oracle exchange issue

--- oracle.sol
+++ oracle.sol
@@ -5,7 +5,12 @@ pragma solidity ^0.4.24;
 contract Oracle {
 
     address public controller;
-    mapping(address => uint) public rate;
+    mapping(address => Rate) public rate;
+
+    struct Rate {
+        uint value;
+        uint decimal;
+    }
 
     constructor() public {
         controller = msg.sender;
@@ -17,13 +22,24 @@ contract Oracle {
         _;
     }
 
-    /// @dev Sets the exchange rates for the selected tokens.
+    function convert(address _token, uint _amount) public view returns (uint) {
+        Rate memory r = rate[_token];
+        uint result = _amount * r.value / 10**r.decimal;
+        if (result == 0) {
+            return 1;
+        }
+        return result;
+    }
+
+    /// @dev Sets the exchange rate for the selected tokens.
     /// @param _tokens a list of ERC20 tokens (matched with _values by index).
-    /// @param _values a list of corresponding exchange values in wei per base unit of token (e.g. 25000000 wei / 0.00000001 TKN).
-    function setRate(address[] _tokens, uint[] _values) public onlyController {
+    /// @param _decimals a list of corresponding decimal places for the token.
+    /// @param _values a list of corresponding exchange values in wei per unit of token (e.g. 25000000 wei / 1 TKN).
+    function set(address[] _tokens, uint[] _values, uint[] _decimals) public onlyController {
         require(_tokens.length == _values.length);
         for (uint i = 0; i < _tokens.length; i++) {
-            rate[_tokens[i]] = _values[i];
+            rate[_tokens[i]] = Rate(_values[i], _decimals[i]);
+
         }
     }
 }
