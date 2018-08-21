fa18027ebc44642184f71f657ba2f91760960d26
Daniel C <nostdm@users.noreply.github.com>
2018-08-21 11:54:32 +0100

    Move contracts to a new directory and separate old contracts (card & controller) from new ones. (#54)
    * Update build script and regenerate bindings
    
    * Fix tests
    
    * Fix tests
    
    * Use combined.json per contract

--- build.sh
+++ build.sh
@@ -1,30 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
-SOLC="docker run -v$PWD:/solidity ethereum/solc:0.4.24"
+SOLC="docker run --rm -v $PWD:/solidity --workdir /solidity/contracts ethereum/solc:0.4.24"
 
 # Compile solidity contracts and hex prefix bytecode string.
-$SOLC --overwrite  --bin --abi wallet.sol     -o ./build/wallet     && bin=$(awk '{print "0x" $0}' ./build/wallet/Wallet.bin)         && echo $bin > ./build/wallet/Wallet.bin
-$SOLC --overwrite  --bin --abi controller.sol -o ./build/controller && bin=$(awk '{print "0x" $0}' ./build/controller/Controller.bin) && echo $bin > ./build/controller/Controller.bin
-$SOLC --overwrite  --bin --abi token.sol      -o ./build/token      && bin=$(awk '{print "0x" $0}' ./build/token/Token.bin)           && echo $bin > ./build/token/Token.bin
-$SOLC --overwrite  --bin --abi card.sol       -o ./build/card       && bin=$(awk '{print "0x" $0}' ./build/card/Card.bin)             && echo $bin > ./build/card/Card.bin
-$SOLC --overwrite  --bin --abi oracle.sol     -o ./build/oracle     && bin=$(awk '{print "0x" $0}' ./build/oracle/Oracle.bin)         && echo $bin > ./build/oracle/Oracle.bin
+${SOLC} --overwrite  --bin --abi wallet.sol         -o /solidity/build/wallet     --combined-json bin-runtime,srcmap-runtime,ast,srcmap,bin && bin=$(awk '{print "0x" $0}' ./build/wallet/Wallet.bin)         && echo $bin > ./build/wallet/Wallet.bin         && echo "Compiled wallet."
+${SOLC} --overwrite  --bin --abi token.sol          -o /solidity/build/token      --combined-json bin-runtime,srcmap-runtime,ast,srcmap,bin && bin=$(awk '{print "0x" $0}' ./build/token/Token.bin)           && echo $bin > ./build/token/Token.bin           && echo "Compiled token."
+${SOLC} --overwrite  --bin --abi oracle.sol         -o /solidity/build/oracle     --combined-json bin-runtime,srcmap-runtime,ast,srcmap,bin && bin=$(awk '{print "0x" $0}' ./build/oracle/Oracle.bin)         && echo $bin > ./build/oracle/Oracle.bin         && echo "Compiled oracle."
+${SOLC} --overwrite  --bin --abi old/card.sol       -o /solidity/build/card       --combined-json bin-runtime,srcmap-runtime,ast,srcmap,bin && bin=$(awk '{print "0x" $0}' ./build/card/Card.bin)             && echo $bin > ./build/card/Card.bin             && echo "Compiled card."
+${SOLC} --overwrite  --bin --abi old/controller.sol -o /solidity/build/controller --combined-json bin-runtime,srcmap-runtime,ast,srcmap,bin && bin=$(awk '{print "0x" $0}' ./build/controller/Controller.bin) && echo $bin > ./build/controller/Controller.bin && echo "Compiled controller."
 
-# create combined-json for code coverage
-$SOLC --overwrite --combined-json bin-runtime,srcmap-runtime,ast,srcmap,bin -o ./build wallet.sol controller.sol token.sol card.sol oracle.sol
-
-GE_PATH="$PWD"/vendor/github.com/ethereum/go-ethereum
-if [ ! -d "$GE_PATH" ]
+GE_PATH=${PWD}/vendor/github.com/ethereum/go-ethereum
+if [ ! -d ${GE_PATH} ]
 then
   GE_PATH=${GOPATH:-$HOME/go}/src/github.com/ethereum/go-ethereum
 fi
 
 # Generate Go bindings from solidity contracts.
-ABIGEN="docker run --workdir /go/src/github/tokencard/contracts -e GOPATH=/go -v "$GE_PATH":/go/src/github.com/ethereum/go-ethereum -v "$PWD":/go/src/github/tokencard/contracts ethereum/client-go:alltools-v1.8.12 abigen"
+ABIGEN="docker run --rm --workdir /go/src/github/tokencard/contracts -e GOPATH=/go -v $GE_PATH:/go/src/github.com/ethereum/go-ethereum -v $PWD:/go/src/github/tokencard/contracts ethereum/client-go:alltools-v1.8.12 abigen"
 
-$ABIGEN --abi ./build/wallet/Wallet.abi         --bin ./build/wallet/Wallet.bin         --pkg bindings --type=Wallet     --out ./pkg/bindings/wallet.go
-$ABIGEN --abi ./build/card/Card.abi             --bin ./build/card/Card.bin             --pkg bindings --type=Card       --out ./pkg/bindings/card.go
-$ABIGEN --abi ./build/oracle/Oracle.abi         --bin ./build/oracle/Oracle.bin         --pkg bindings --type=Oracle     --out ./pkg/bindings/oracle.go
-$ABIGEN --abi ./build/controller/Controller.abi --bin ./build/controller/Controller.bin --pkg bindings --type=Controller --out ./pkg/bindings/controller.go
-$ABIGEN --abi ./build/token/Token.abi           --bin ./build/token/Token.bin           --pkg bindings --type=Token      --out ./pkg/bindings/token.go
+${ABIGEN} --abi ./build/wallet/Wallet.abi         --bin ./build/wallet/Wallet.bin         --pkg bindings --type=Wallet     --out ./pkg/bindings/wallet.go     && echo "Generated wallet bindings."
+${ABIGEN} --abi ./build/card/Card.abi             --bin ./build/card/Card.bin             --pkg bindings --type=Card       --out ./pkg/bindings/card.go       && echo "Generated card bindings."
+${ABIGEN} --abi ./build/oracle/Oracle.abi         --bin ./build/oracle/Oracle.bin         --pkg bindings --type=Oracle     --out ./pkg/bindings/oracle.go     && echo "Generated oracle bindings."
+${ABIGEN} --abi ./build/controller/Controller.abi --bin ./build/controller/Controller.bin --pkg bindings --type=Controller --out ./pkg/bindings/controller.go && echo "Generated controller bindings."
+${ABIGEN} --abi ./build/token/Token.abi           --bin ./build/token/Token.bin           --pkg bindings --type=Token      --out ./pkg/bindings/token.go      && echo "Generated token bindings."
