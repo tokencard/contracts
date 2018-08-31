87354c967ba6ce4d5f11c7d72f28429274ca07ed
Yannis Stamelakos <yannis@Yanniss-MacBook-Pro.local>
2018-08-31 16:46:00 +0100

    Restructure and add event emission test

--- test/oracleV2/add_token_test.go
+++ test/oracleV2/add_token_test.go
@@ -7,32 +7,62 @@ import (
 )
 
 var _ = Describe("addToken", func() {
+
 	Context("When called by the controller", func() {
-		Context("When the token is not already supported", func() {
+		Context("When the token is not supported", func() {
 			It("Should not fail", func() {
-				tx, err := oracle.AddToken(controllerWallet.TransactOpts(), common.HexToAddress("0x1"), "ETH", 8)
+				tx, err := oracle.AddToken(controllerWallet.TransactOpts(), common.HexToAddress("0x0"), "ETH", 18)
 				Expect(err).ToNot(HaveOccurred())
 				be.Commit()
 				Expect(isSuccessful(tx)).To(BeTrue())
 			})
+
 		})
-		Context("When the token is already supported", func() {
+		Context("When the token is added/supported", func() {
 			BeforeEach(func() {
-				tx, err := oracle.AddToken(controllerWallet.TransactOpts(), common.HexToAddress("0x1"), "ETH", 8)
+				tx, err := oracle.AddToken(controllerWallet.TransactOpts(), common.HexToAddress("0x0"), "ETH", 18)
 				Expect(err).ToNot(HaveOccurred())
 				be.Commit()
 				Expect(isSuccessful(tx)).To(BeTrue())
+			})
+
+			It("Should emit an TokenAddition event", func() {
+				// s := make([]common.Address)
+				// s.append(common.HexToAddress("0x0"))
+				it, err := oracle.FilterTokenAddition(nil, []common.Address{common.HexToAddress("0x0")})
+				Expect(err).ToNot(HaveOccurred())
+				Expect(it.Next()).To(BeTrue())
+				evt := it.Event
+				Expect(it.Next()).To(BeFalse())
+				Expect(evt.TokenID).To(Equal(common.HexToAddress("0x0")))
+				Expect(evt.Label).To(Equal("ETH"))
 			})
 
 			It("Should fail", func() {
 				to := controllerWallet.TransactOpts()
 				to.GasLimit = 100000
-				tx, err := oracle.AddToken(to, common.HexToAddress("0x1"), "ETH", 8)
+				tx, err := oracle.AddToken(to, common.HexToAddress("0x0"), "ETH", 18)
 				Expect(err).ToNot(HaveOccurred())
 				be.Commit()
+
 				Expect(isSuccessful(tx)).To(BeFalse())
+
 			})
+		})
+
+	})
+
+	Context("When called by a random address", func() {
+		It("Should fail", func() {
+			to := randomWallet.TransactOpts()
+			to.GasLimit = 300000
+			tx, err := oracle.AddToken(to, common.HexToAddress("0x0"), "ETH", 18)
+			Expect(err).ToNot(HaveOccurred())
+			be.Commit()
+			Expect(isGasExausted(tx, to.GasLimit)).To(BeFalse())
+			Expect(isSuccessful(tx)).To(BeFalse())
 
 		})
 	})
+
 })
