d719bbe91b9c5093388d19a1dab6c2f75ea795de
Mischa Tuffield <mischa@mmt.me.uk>
2018-09-13 22:44:34 +0100

    Updates imports

--- pkg/bindings/wallet.go
+++ pkg/bindings/wallet.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/event"
 )
 
