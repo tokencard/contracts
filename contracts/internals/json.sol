afce0f6fc6ee7f951ee49c718f14b93dad64ad1e
Mischa Tuffield <mischa@mmt.me.uk>
2018-10-27 13:36:15 +0100

    This adds in some more licensing stuff

--- contracts/internals/json.sol
+++ contracts/internals/json.sol
@@ -1,3 +1,21 @@
+/**
+ *  TokenWallet - The Consumer Contract Wallet
+ *  Copyright (C) 2018 Token Group Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 pragma solidity ^0.4.25;
 
 import "../externals/strings.sol";
@@ -6,9 +24,9 @@ import "../externals/strings.sol";
 contract JSON {
     using strings for *;
 
-    // @dev Extracts JSON rate value from the response object.
-    // @param _json body of the JSON response from the CryptoCompare API.
-    // @param _symbol asset symbol used to extract the correct json field.
+    /// @dev Extracts JSON rate value from the response object.
+    /// @param _json body of the JSON response from the CryptoCompare API.
+    /// @param _symbol asset symbol used to extract the correct json field.
     function parseRate(string _json, string _symbol) internal pure returns (string) {
         strings.slice memory body = _json.toSlice();
         body.beyond("{".toSlice());
--- contracts/internals/ownable.sol
+++ contracts/internals/ownable.sol
@@ -1,3 +1,21 @@
+/**
+ *  TokenWallet - The Consumer Contract Wallet
+ *  Copyright (C) 2018 Token Group Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 pragma solidity ^0.4.25;
 
 
@@ -8,21 +26,21 @@ contract Ownable {
     address private _owner;
     bool private _isTransferable;
 
-    // @dev Constructor sets the original owner of the contract and whether or not it is one time transferable.
+    /// @dev Constructor sets the original owner of the contract and whether or not it is one time transferable.
     constructor(address _account, bool _transferable) internal {
         _owner = _account;
         _isTransferable = _transferable;
         emit TransferredOwnership(address(0), _account);
     }
 
-    // @dev Reverts if called by any account other than the owner.
+    /// @dev Reverts if called by any account other than the owner.
     modifier onlyOwner() {
         require(isOwner(), "sender is not an owner");
         _;
     }
 
-    // @dev Allows the current owner to transfer control of the contract to a new address.
-    // @param _account address to transfer ownership to.
+    /// @dev Allows the current owner to transfer control of the contract to a new address.
+    /// @param _account address to transfer ownership to.
     function transferOwnership(address _account) external onlyOwner {
         // Require that the ownership is transferable.
         require(_isTransferable, "ownership is not transferable");
@@ -36,9 +54,9 @@ contract Ownable {
         _owner = _account;
     }
 
-    // @dev Allows the current owner to relinquish control of the contract.
-    // @notice Renouncing to ownership will leave the contract without an owner and unusable.
-    // It will not be possible to call the functions with the `onlyOwner` modifier anymore.
+    /// @dev Allows the current owner to relinquish control of the contract.
+    /// @notice Renouncing to ownership will leave the contract without an owner and unusable.
+    /// It will not be possible to call the functions with the `onlyOwner` modifier anymore.
     function renounceOwnership() public onlyOwner {
         // Require that the ownership is transferable.
         require(_isTransferable, "ownership is not transferable");
@@ -47,17 +65,17 @@ contract Ownable {
         _owner = address(0);
     }
 
-    // @return the address of the owner.
+    /// @return the address of the owner.
     function owner() public view returns (address) {
         return _owner;
     }
 
-    // @return true if the ownership is transferable.
+    /// @return true if the ownership is transferable.
     function isTransferable() public view returns (bool) {
         return _isTransferable;
     }
 
-    // @return true if sender is the owner of the contract.
+    /// @return true if sender is the owner of the contract.
     function isOwner() internal view returns (bool) {
         return msg.sender == _owner;
     }
