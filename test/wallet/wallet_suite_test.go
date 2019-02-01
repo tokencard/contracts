21f3df9180ad707147e3525e6a3dce59d4536e7f
Mischa Tuffield <mischa@mmt.me.uk>
2019-02-01 23:57:58 +0000

    This cleans up the wallet tests

--- test/wallet/wallet_suite_test.go
+++ test/wallet/wallet_suite_test.go
@@ -36,7 +36,7 @@ var _ = BeforeEach(func() {
 })
 
 var _ = AfterSuite(func() {
-	TestRig.ExpectMinimumCoverage("wallet.sol", 95.54)
+	TestRig.ExpectMinimumCoverage("wallet.sol", 100.00)
 	TestRig.PrintGasUsage(os.Stdout)
 })
 
