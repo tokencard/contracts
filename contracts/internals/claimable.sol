de461e8ffaad3beeeefd1d23b80eb682ac79d14c
Mischa Tuffield <mischa@mmt.me.uk>
2019-02-05 17:47:55 +0000

    Made this claimable and fixed tests

---
+++ contracts/internals/claimable.sol
@@ -0,0 +1,41 @@
+/**
+ *  The Consumer Contract Wallet
+ *  Copyright (C) 2018 Token Group Ltd
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pragma solidity ^0.4.25;
+
+import "../externals/ERC20.sol";
+
+/// @title Withdrawable to withdraw tokens accidentally sent to a contract
+contract Claimable {
+
+    event Claimed(address _to, address _asset, uint _amount);
+
+    /// @dev This function is used to move tokens sent accidentally to this contract method.
+    /// @dev The owner can chose the new destination address
+    /// @param _to is the recipient's address.
+    /// @param _asset is the address of an ERC20 token or 0x0 for ether.
+    /// @param _amount is the amount to be transferred in base units.
+    function _claim(address _to, address _asset, uint _amount) internal {
+        if (_asset == address(0)) {
+          _to.transfer(_amount);
+        } else {
+          require(ERC20(_asset).transfer(_to, _amount), "ERC20 token transfer was unsuccessful");
+        }
+        emit Claimed(_to, _asset, _amount);
+    }
+}
