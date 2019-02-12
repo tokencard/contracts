6f8845ae2cbb215b9d8d7fe1ea7a9619100cb816
Mischa Tuffield <mischa@mmt.me.uk>
2019-02-12 22:15:43 +0000

    This updates the bindings in master

--- pkg/bindings/internals/parseIntScientific.go
+++ pkg/bindings/internals/parseIntScientific.go
@@ -31,7 +31,7 @@ var (
 const ParseIntScientificABI = "[]"
 
 // ParseIntScientificBin is the compiled bytecode used for deploying new contracts.
-const ParseIntScientificBin = `6080604052348015600f57600080fd5b50603580601d6000396000f3006080604052600080fd00a165627a7a72305820f8df5a5c5be4bbc862fed51905695a927b9cefb28e39da6747747b6272d7ebce0029`
+const ParseIntScientificBin = `6080604052348015600f57600080fd5b50603580601d6000396000f3006080604052600080fd00a165627a7a72305820ba23090bb403af51c06d44238ffa802a075c892f5684fdd0271b71625b57ec7b0029`
 
 // DeployParseIntScientific deploys a new Ethereum contract, binding an instance of ParseIntScientific to it.
 func DeployParseIntScientific(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *ParseIntScientific, error) {
