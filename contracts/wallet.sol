3cc366a3f30b9f6b7b5cd8b53e1b2c34d356c7dc
i-stam <i.stamelakos@gmail.com>
2019-05-03 14:43:12 +0100

    Replace external call with new version

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -739,6 +739,7 @@ contract Wallet is ENSResolvable, Vault, GasTopUpLimit, LoadLimit {
         return success;
     }
 
+
     /// @dev This function converts to an address
     /// @param _bts bytes
     /// @param _from start position
