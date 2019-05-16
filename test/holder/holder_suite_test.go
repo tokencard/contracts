53a4299420336629b192bf81d5849b07c250ec51
i-stam <i.stamelakos@gmail.com>
2019-05-16 18:09:28 +0100

    Add initial holder tests

---
+++ test/holder/holder_suite_test.go
@@ -0,0 +1,64 @@
+package holder_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/tokencard/contracts/test/shared"
+)
+
+func init() {
+	TestRig.AddCoverageForContracts(
+		"../../build/holder/combined.json",
+		"../../contracts")
+
+	TestRig.AddCoverageForContracts(
+		"../../build/mocks/burnerToken/combined.json",
+		"../../contracts")
+}
+
+func TestHolderSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Contract Suite")
+}
+
+var _ = BeforeEach(func() {
+	err := InitializeBackend()
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	TestRig.ExpectMinimumCoverage("holder.sol", 0.00)
+	TestRig.ExpectMinimumCoverage("mocks/tkn.sol", 10.00)
+	TestRig.PrintGasUsage(os.Stdout)
+})
+
+func isSuccessful(tx *types.Transaction) bool {
+	r, err := Backend.TransactionReceipt(context.Background(), tx.Hash())
+	Expect(err).ToNot(HaveOccurred())
+	return r.Status == types.ReceiptStatusSuccessful
+}
+
+func isGasExhausted(tx *types.Transaction, gasLimit uint64) bool {
+	r, err := Backend.TransactionReceipt(context.Background(), tx.Hash())
+	Expect(err).ToNot(HaveOccurred())
+	if r.Status == types.ReceiptStatusSuccessful {
+		return false
+	}
+	return r.GasUsed == gasLimit
+}
+
+var _ = AfterEach(func() {
+	td := CurrentGinkgoTestDescription()
+	if td.Failed {
+		fmt.Fprintf(GinkgoWriter, "\nLast Executed Smart Contract Line for %s:%d\n", td.FileName, td.LineNumber)
+		fmt.Fprintln(GinkgoWriter, TestRig.LastExecuted())
+	}
+	err := Backend.Close()
+	Expect(err).ToNot(HaveOccurred())
+})
