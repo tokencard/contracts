091e5c44bc87651c994ffd88310146e57043e075
Mischa Tuffield <mischa@mmt.me.uk>
2019-07-09 01:18:07 +0100

    This removes the mock token from our contracts, it is kept for testing only

---
+++ contracts/internals/burner.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.7;
+
+// The BurnerToken interface is the interface to a token contract which
+// provides the total burnable supply for the TokenHolder contract.
+interface Burner {
+    function currentSupply() external view returns (uint);
+}
+
