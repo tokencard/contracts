2a452c4bc1d2ca73068264ca4812d10e8e348b65
Mischa Tuffield <mischa@mmt.me.uk>
2019-07-09 22:46:09 +0100

    WIP changes to the migrate ... to ensure we preserve our user's security settings

--- contracts/wallet-deployer.sol
+++ contracts/wallet-deployer.sol
@@ -1,6 +1,6 @@
 /**
  *  The Consumer Contract Wallet Deployer
- *  Copyright (C) 2018 The Contract Wallet Company Limited
+ *  Copyright (C) 2019 The Contract Wallet Company Limited
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,7 +22,9 @@ import "./wallet.sol";
 import "./internals/controllable.sol";
 
 
-//// @title Wallet deployer with pre-caching if wallets functionality.
+/// @title Wallet Deployer
+/// @notice Factory method to create deploy a new wallet
+/// @notice Allows for pre-fabrication / caching of wallets
 contract WalletDeployer is Controllable {
 
     event CachedWallet(address wallet);
@@ -37,6 +39,7 @@ contract WalletDeployer is Controllable {
     bytes32 public oracleName;
     uint public defaultSpendLimit;
 
+    /// @notice parameters are passed in so that they can be used to construct new instances of the wallet
     constructor(address _ens, bytes32 _oracleName, bytes32 _controllerName, uint _spendLimit) Controllable(_ens, _controllerName) public {
         ens = _ens;
         controllerName = _controllerName;
@@ -44,39 +47,56 @@ contract WalletDeployer is Controllable {
         defaultSpendLimit = _spendLimit;
     }
 
+    /// @notice This function is used to deploy a Wallet for a given owner address
+    /// @param owner is the owner address for the new Wallet to be
     function deployWallet(address owner) external onlyController {
         if (cached.length < 1) {
             cacheWallet();
     	}
+
         address walletAddress = cached[cached.length-1];
         cached.length--;
         Wallet(walletAddress).transferOwnership(owner);
         deployed[owner] = walletAddress;
+
         emit DeployedWallet(walletAddress, owner);
     }
 
-    function migrateWallet(address owner, uint _spendLimit, address[] _whitelistedAddresses) external onlyController {
+    /// @notice This function is used to migrate an owner's security settings from a previous version of the wallet
+    /// @param owner is the owner address for the new Wallet to be
+    /// @param _spendLimit is the user's set daily spend limit
+    /// @param _topUpLimit is the user's set daily gas top-up limit
+    /// @param _whitelistedAddresses is the set of the user's whitelisted addresses
+    function migrateWallet(address owner, uint _spendLimit, uint _topUpLimit, bool _initializedWhitelist, address[] _whitelistedAddresses) external onlyController {
         if (cached.length < 1) {
             cacheWallet();
     	}
+
         address walletAddress = cached[cached.length-1];
         cached.length--;
-        deployed[owner] = walletAddress;
+
         Wallet(walletAddress).initializeSpendLimit(_spendLimit);
-        Wallet(walletAddress).initializeWhitelist(_whitelistedAddresses);
+        Wallet(walletAddress).initializeTopUpLimit(_topUpLimit);
+
+        if (_initializedWhitelist) {
+            Wallet(walletAddress).initializeWhitelist(_whitelistedAddresses);
+        }
+
         Wallet(walletAddress).transferOwnership(owner);
+        deployed[owner] = walletAddress;
+
         emit MigratedWallet(walletAddress, owner);
     }
 
+    /// @notice returns the number of pre-cached wallets
     function cachedWalletCount() external view returns (uint) {
         return cached.length;
     }
 
+    /// @notice This public method allows anyone to pre-cache wallets
     function cacheWallet() public {
         address walletAddress = new Wallet(address(this), true, ens, oracleName, controllerName, defaultSpendLimit);
         cached.push(walletAddress);
         emit CachedWallet(walletAddress);
     }
-
-
 }
