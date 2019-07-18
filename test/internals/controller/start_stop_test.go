f9f42db61b79611af1d81ebe3a895239d4ee5703
i-stam <i.stamelakos@gmail.com>
2019-07-18 12:07:50 +0100

    Add event emission tests

--- test/internals/controller/start_stop_test.go
+++ test/internals/controller/start_stop_test.go
@@ -42,42 +42,94 @@ var _ = Describe("Controller Stopping", func() {
 			Expect(stopped).To(BeTrue())
 		})
 
-	})
-
-	When("a random address tries to start", func() {
-		It("Should fail", func() {
-			tx, err := ControllerContract.Start(RandomAccount.TransactOpts(ethertest.WithGasLimit(1000000)))
+        It("should emit Stopped event", func() {
+			it, err := ControllerContract.FilterStopped(nil)
 			Expect(err).ToNot(HaveOccurred())
-			Backend.Commit()
-			Expect(isSuccessful(tx)).To(BeFalse())
+			Expect(it.Next()).To(BeTrue())
+			evt := it.Event
+			Expect(it.Next()).To(BeFalse())
+			Expect(evt.Sender).To(Equal(ControllerAdmin.Address()))
 		})
+
 	})
 
-	When("controller tries to start", func() {
-		It("Should fail", func() {
-			tx, err := ControllerContract.Start(Controller.TransactOpts(ethertest.WithGasLimit(1000000)))
-			Expect(err).ToNot(HaveOccurred())
-			Backend.Commit()
-			Expect(isSuccessful(tx)).To(BeFalse())
-		})
-	})
+    When("a random address tries to start", func() {
+        It("Should fail", func() {
+            tx, err := ControllerContract.Start(RandomAccount.TransactOpts(ethertest.WithGasLimit(1000000)))
+            Expect(err).ToNot(HaveOccurred())
+            Backend.Commit()
+            Expect(isSuccessful(tx)).To(BeFalse())
+        })
+    })
 
-	When("controller admin tries to start", func() {
-		It("Should fail", func() {
-			tx, err := ControllerContract.Start(ControllerAdmin.TransactOpts(ethertest.WithGasLimit(1000000)))
-			Expect(err).ToNot(HaveOccurred())
-			Backend.Commit()
-			Expect(isSuccessful(tx)).To(BeFalse())
-		})
-	})
+    When("controller tries to start", func() {
+        It("Should fail", func() {
+            tx, err := ControllerContract.Start(Controller.TransactOpts(ethertest.WithGasLimit(1000000)))
+            Expect(err).ToNot(HaveOccurred())
+            Backend.Commit()
+            Expect(isSuccessful(tx)).To(BeFalse())
+        })
+    })
 
-	When("owner tries to start", func() {
-		It("Should pass", func() {
-			tx, err := ControllerContract.Start(ControllerOwner.TransactOpts())
+    When("controller admin tries to start", func() {
+        It("Should fail", func() {
+            tx, err := ControllerContract.Start(ControllerAdmin.TransactOpts(ethertest.WithGasLimit(1000000)))
+            Expect(err).ToNot(HaveOccurred())
+            Backend.Commit()
+            Expect(isSuccessful(tx)).To(BeFalse())
+        })
+    })
+
+    When("the owner stops the controller", func() {
+
+		BeforeEach(func() {
+			tx, err := ControllerContract.Stop(ControllerOwner.TransactOpts())
 			Expect(err).ToNot(HaveOccurred())
 			Backend.Commit()
 			Expect(isSuccessful(tx)).To(BeTrue())
 		})
-	})
+
+        It("The controller should be stopped", func() {
+			stopped, err := ControllerContract.IsStopped(nil)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(stopped).To(BeTrue())
+		})
+
+        It("should emit Stopped event", func() {
+			it, err := ControllerContract.FilterStopped(nil)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(it.Next()).To(BeTrue())
+			evt := it.Event
+			Expect(it.Next()).To(BeFalse())
+			Expect(evt.Sender).To(Equal(ControllerOwner.Address()))
+		})
+
+        When("owner tries to start", func() {
+    		BeforeEach(func() {
+    			tx, err := ControllerContract.Start(ControllerOwner.TransactOpts())
+    			Expect(err).ToNot(HaveOccurred())
+    			Backend.Commit()
+    			Expect(isSuccessful(tx)).To(BeTrue())
+    		})
+
+            It("The controller should start again", func() {
+    			stopped, err := ControllerContract.IsStopped(nil)
+    			Expect(err).ToNot(HaveOccurred())
+    			Expect(stopped).To(BeFalse())
+    		})
+
+            It("should emit Started event", func() {
+    			it, err := ControllerContract.FilterStarted(nil)
+    			Expect(err).ToNot(HaveOccurred())
+    			Expect(it.Next()).To(BeTrue())
+    			evt := it.Event
+    			Expect(it.Next()).To(BeFalse())
+    			Expect(evt.Sender).To(Equal(ControllerOwner.Address()))
+    		})
+    	})
+
+    })
+
+
 
 })
