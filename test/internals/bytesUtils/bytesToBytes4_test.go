2d2c0a1dcc0bd29fe27e992060f7e329b980016b
i-stam <i.stamelakos@gmail.com>
2019-08-04 16:37:24 +0100

    Fix bytesToBytes4 tests

--- test/internals/bytesUtils/bytesToBytes4_test.go
+++ test/internals/bytesUtils/bytesToBytes4_test.go
@@ -2,18 +2,21 @@ package bytesUtils_test
 
 import (
     "math/big"
-
+    "encoding/binary"
     "github.com/ethereum/go-ethereum/common"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("BytesToUint32", func() {
+var _ = Describe("BytesToBytes4", func() {
+
+        bytes4slice := make([]byte, 4)
+        var bytes4 [4]byte
 
 		When("the length is less than 4 bytes", func() {
 			It("Should revert", func() {
                 bytes := common.Hex2Bytes("000000")
-                _, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(0))
+                _, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(0))
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("slicing out of range"))
 			})
@@ -22,7 +25,7 @@ var _ = Describe("BytesToUint32", func() {
         When("the length is 4 bytes but the start index is not zero", func() {
 			It("Should revert", func() {
                 bytes := common.Hex2Bytes("00000000")
-				_, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(1))
+				_, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(1))
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("slicing out of range"))
 			})
@@ -30,43 +33,55 @@ var _ = Describe("BytesToUint32", func() {
 
         It("should return 0", func() {
             bytes := common.Hex2Bytes("00000000")
-			u32, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(0))
+			b4, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(0))
 			Expect(err).ToNot(HaveOccurred())
-			Expect(u32).To(Equal(uint32(0)))
+            binary.BigEndian.PutUint32(bytes4slice, 0)
+            copy(bytes4[:], bytes4slice)
+			Expect(b4).To(Equal(bytes4))
 		})
 
 		It("should return 1", func() {
             bytes := common.Hex2Bytes("00000001")
-			u32, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(0))
+			b4, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(0))
 			Expect(err).ToNot(HaveOccurred())
-			Expect(u32).To(Equal(uint32(1)))
+            binary.BigEndian.PutUint32(bytes4slice, 1)
+            copy(bytes4[:], bytes4slice)
+			Expect(b4).To(Equal(bytes4))
 		})
 
         It("should return 1", func() {
             bytes := common.Hex2Bytes("ffffffff00000001ffffffff")
-			u32, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(4))
+			b4, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(4))
 			Expect(err).ToNot(HaveOccurred())
-			Expect(u32).To(Equal(uint32(1)))
+            binary.BigEndian.PutUint32(bytes4slice, 1)
+            copy(bytes4[:], bytes4slice)
+			Expect(b4).To(Equal(bytes4))
 		})
 
         It("should return  2^32-1", func() {
             bytes := common.Hex2Bytes("ffffffff")
-			u32, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(0))
+			b4, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(0))
 			Expect(err).ToNot(HaveOccurred())
-			Expect(u32).To(Equal(uint32(4294967295)))
+            binary.BigEndian.PutUint32(bytes4slice, 4294967295)
+            copy(bytes4[:], bytes4slice)
+			Expect(b4).To(Equal(bytes4))
 		})
 
         It("should return the given random number", func() {
             bytes := common.Hex2Bytes("abcdef89")
-			u32, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(0))
+			b4, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(0))
 			Expect(err).ToNot(HaveOccurred())
-			Expect(u32).To(Equal(uint32(2882400137)))
+            binary.BigEndian.PutUint32(bytes4slice, 2882400137)
+            copy(bytes4[:], bytes4slice)
+			Expect(b4).To(Equal(bytes4))
 		})
 
         It("should return the given random number", func() {
             bytes := common.Hex2Bytes("ffffffffffabcdef89ffff")
-			u32, err := BytesUtilsExporter.BytesToUint32(nil, bytes, big.NewInt(5))
+			b4, err := BytesUtilsExporter.BytesToBytes4(nil, bytes, big.NewInt(5))
 			Expect(err).ToNot(HaveOccurred())
-			Expect(u32).To(Equal(uint32(2882400137)))
+            binary.BigEndian.PutUint32(bytes4slice, 2882400137)
+            copy(bytes4[:], bytes4slice)
+			Expect(b4).To(Equal(bytes4))
 		})
 })
