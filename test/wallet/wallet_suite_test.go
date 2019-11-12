38e46c36e44e4dae3aeec17520bb7bd24c555aeb
Mischa Tuffield <mischa@mmt.me.uk>
2019-11-12 17:57:46 +0000

    This cleans up the version number

--- test/wallet/wallet_suite_test.go
+++ test/wallet/wallet_suite_test.go
@@ -49,7 +49,7 @@ var _ = BeforeEach(func() {
 })
 
 var allPassed = true
-var currentVersion = "2.2.0"
+var currentVersion = "2.3.0"
 
 var _ = Describe("Wallet Version", func() {
 	It("should return the current version", func() {
