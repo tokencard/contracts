0c44bd06b9e3a981811d91b71e6e3334616e9297
i-stam <i.stamelakos@gmail.com>
2020-01-09 23:09:57 +0000

    Consolidate spendLimit and loadLimit

--- contracts/walletDeployer.sol
+++ contracts/walletDeployer.sol
@@ -72,7 +72,7 @@ contract WalletDeployer is ENSResolvable, Controllable {
 
         // Sets up the security settings as per the _oldWallet
         if (_initializedSpendLimit) {
-            wallet.setSpendLimit(_spendLimit);
+            wallet.setDailyLimit(_spendLimit);
         }
         if (_initializedWhitelist) {
             wallet.setWhitelist(_whitelistedAddresses);
