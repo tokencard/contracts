d652fdc5926536affeb6aa461dfa25dad25192cd
i-stam <i.stamelakos@gmail.com>
2020-05-07 00:04:18 +0100

    Use solc optimizer for echidna and manticore

--- tools/manticore/manticore.sh
+++ tools/manticore/manticore.sh
@@ -6,10 +6,10 @@ if [ ! -d "contracts" ]; then
 fi
 
 docker run --rm -v "$PWD":/contracts -it --ulimit stack=100000000:100000000 --workdir=/contracts trailofbits/eth-security-toolbox@sha256:e5e2ebbffcc4c3a334063ba3871ec25a4e1dc4915d77166aef1aa265e0a5978f -c 'solc-select 0.6.4 &&
-manticore contracts/controller.sol --contract=Controller --config=tools/manticore/manticore.yaml --quick-mode &&
-manticore contracts/wallet.sol --contract=Wallet --config=tools/manticore/manticore.yaml --quick-mode &&
-manticore contracts/oracle.sol --contract=Oracle --config=tools/manticore/manticore.yaml --quick-mode &&
-manticore contracts/tokenWhitelist.sol --contract=TokenWhitelist --config=tools/manticore/manticore.yaml --quick-mode &&
-manticore contracts/licence.sol --contract=Licence --config=tools/manticore/manticore.yaml --quick-mode &&
-manticore contracts/holder.sol --contract=Holder --config=tools/manticore/manticore.yaml --quick-mode &&
-manticore contracts/walletDeployer.sol --contract=WalletDeployer --config=tools/manticore/manticore.yaml --quick-mode'
+manticore contracts/controller.sol --contract=Controller --solc-args="--optimize" --config=tools/manticore/manticore.yaml --quick-mode &&
+manticore contracts/wallet.sol --contract=Wallet --solc-args="--optimize" --config=tools/manticore/manticore.yaml --quick-mode &&
+manticore contracts/oracle.sol --contract=Oracle --solc-args="--optimize" --config=tools/manticore/manticore.yaml --quick-mode &&
+manticore contracts/tokenWhitelist.sol --contract=TokenWhitelist --solc-args="--optimize" --config=tools/manticore/manticore.yaml --quick-mode &&
+manticore contracts/licence.sol --contract=Licence --solc-args="--optimize" --config=tools/manticore/manticore.yaml --quick-mode &&
+manticore contracts/holder.sol --contract=Holder --solc-args="--optimize" --config=tools/manticore/manticore.yaml --quick-mode &&
+manticore contracts/walletDeployer.sol --contract=WalletDeployer --solc-args="--optimize" --config=tools/manticore/manticore.yaml --quick-mode'
