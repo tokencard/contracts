759b78c872045681f5b6b44d95f4c24ff15e2849
Riccardo Persiani <r.persiani92@gmail.com>
2020-11-09 18:18:18 +0100

    Fix upgradeable tests

--- test/upgradeable/uninitialized_test.go
+++ test/upgradeable/uninitialized_test.go
@@ -35,12 +35,12 @@ var _ = Describe("uninitialized", func() {
 		})
 
 		It("Should fail it when there's onlyOwnerOrSelf()", func() {
-			tx, err = ProxyWallet.SetDailyLimit(Owner.TransactOpts(ethertest.WithGasLimit(100000)), big.NewInt(1000))
+			tx, err = ProxyWallet.SubmitDailyLimitUpdate(Owner.TransactOpts(ethertest.WithGasLimit(100000)), big.NewInt(1000))
 			Expect(err).ToNot(HaveOccurred())
 			Backend.Commit()
 			Expect(isSuccessful(tx)).To(BeFalse())
 			returnData, _ := ethCall(tx)
-			Expect(string(returnData[len(returnData)-64:])).To(ContainSubstring("only owner||self"))
+			Expect(string(returnData[len(returnData)-64:])).To(ContainSubstring("only owner or self"))
 		})
 
 		It("Should fail it when there's onlyController()", func() {
